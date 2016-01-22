155bab944e87e41f76d137481316d33d1b6c50a8
NiramQ <niramrus@gmail.com>
2016-01-22 17:05:00 +0500

    fck it 0.11

--- testkurs/admin/test2.php
+++ testkurs/admin/test2.php
@@ -10,11 +10,12 @@
 $zapros1 = "SELECT idw, idt FROM WT";
 $zapros2 = "SELECT citya, cityb, money, nomer FROM WAYS";
 $ZaprosCitya = "SELECT DISTINCT citya FROM WAYS";
-
+$ZaprosCityb = "SELECT DISTINCT cityb FROM WAYS";
 
 $result = mysql_query($zapros1);
 $result2 = mysql_query($zapros2);
 $result3 = mysql_query($ZaprosCitya);
+$result4 = mysql_query($ZaprosCityb);
 
 if (!$result) {
     echo "Could not successfully run query ($zapros1) from DB: " . mysql_error();
@@ -26,8 +27,8 @@ if (mysql_num_rows($result) == 0) {
     exit;
 }
 while ($row = mysql_fetch_assoc($result)) {
-    echo $row["idw"]."-";
-    echo $row["idt"].",";
+    echo $row["idw"] . "-";
+    echo $row["idt"] . ",";
 }
 ?> </br><?
 if (!$result2) {
@@ -40,10 +41,11 @@ if (mysql_num_rows($result2) == 0) {
     exit;
 }
 while ($row2 = mysql_fetch_assoc($result2)) {
-    echo $row2["citya"]."-";
-    echo $row2["cityb"]."<br>";
-    echo $row2["money"]."<br>";
-    echo $row2["nomer"]."<br>";
+    echo $row2["citya"] . "-";
+    echo $row2["cityb"] . "<br>";
+    echo $row2["money"] . "&nbsp руб.<br>№ &nbsp";
+    echo $row2["nomer"] . "<br>";
+    echo "_______<br>";
 }
 if (!$result3) {
     echo "Could not successfully run query ($ZaprosCitya) from DB: " . mysql_error();
@@ -54,10 +56,59 @@ if (mysql_num_rows($result3) == 0) {
     echo "No rows found, nothing to print so am exiting";
     exit;
 }
+echo "<form method='post' action='test2.php'>";
+echo "<select  name='picka'>";
 while ($row = mysql_fetch_assoc($result3)) {
-    echo $row["citya"]."</br>";
+    echo "<option value=" . $row["citya"] . ">" . $row["citya"] . "</option>";
+};
+echo "</select>";
+
+if (mysql_num_rows($result4) == 0) {
+    echo "No rows found, nothing to print so am exiting";
+    exit;
 }
-?>
+echo "<select name='pickb'>";
+while ($row = mysql_fetch_assoc($result4)) {
+    echo "<option >" . $row["cityb"] . "</option>";
+};
+echo "</select>";
+
+echo "<input type='submit' name='search' value='send'>
+</form>";
+$zapros4 = "SELECT * FROM WAYS WHERE citya='" . $_POST['picka'] . "' AND cityb='" . $_POST['pickb'] . "'"; //выборка городов "из", "в"
+$zaprosid = "SELECT * FROM (WAYS,TIMES,WT) WHERE (citya='" . $_POST['picka'] . "' AND cityb='" . $_POST['pickb'] . "' AND WAYS.id = WT.idw AND TIMES.id = WT.idt)";
+$result5 = mysql_query($zapros4);
+$result6 = mysql_query($zaprosid);
+if (isset($_POST['search']))
+    if (!$result5) {
+        echo "Could not successfully run query ($zapros4) from DB: " . mysql_error();
+        exit;
+    }
+
+if (mysql_num_rows($result5) == 0) {
+    "<br>";
+    echo "маршрут &nbsp" . $_POST['picka'] . "-" . $_POST['pickb'] . "&nbsp отсутствует";
+    exit;
+} else echo $_POST['picka'] . "-" . $_POST['pickb'] . "<br>";
+
+$row = mysql_fetch_assoc($result5);
+echo $row["money"] . "&nbsp руб.,";
+echo "&nbsp № &nbsp" . $row["nomer"]."<br>";
+if (!$result6) {
+    echo "Could not successfully run query ($zaprosid) from DB: " . mysql_error();
+    exit;
+}
+
+if (mysql_num_rows($result6) == 0) {
+    echo "No rows found, nothing to print so am exiting";
+    exit;
+}
+while ($row = mysql_fetch_assoc($result6)) {
+    $date = date_create($row["time"]);
+    echo date_format($date, 'H:i').", &nbsp";
+}
+
+
 ?>
 
 
